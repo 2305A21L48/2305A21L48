2a36c904a7f5ab2cb7f1a80653b31c09a55cb064
2305A21L48 <2305a21l48@sru.edu.in>
2024-11-22 12:07:52 +0530

    Add files via upload

---
+++ 2305A21L48.py
@@ -0,0 +1,29 @@
+#creating a oc transformer model
+import streamlit as st
+import math as m
+
+st.markdown('<h1>2305A21L48 open circuit transformer efficiency</h1>',unsafe_allow_html=True)
+
+with st.form('form2'):
+    col1,col2,col3=st.columns(3)
+    V0=st.number_input('v0(v)')
+    I0=st.number_input('I0(A)')
+    W0=st.number_input('W0(W)')
+    if V0 and I0==0:
+        st.write('invalid inputs')
+    submitted=st.form_submit_button('compute')
+
+if submitted:
+    cos0=W0/(V0*I0)
+    sin0=m.sqrt(1-cos0**2)
+    Iw = I0 * cos0
+    Iu = I0 * sin0
+    if V0 and I0 != 0:
+        R0 = V0 / Iw
+        X0 = V0 / Iu
+        st.write('R0:',R0,'ohm')
+        st.write('X0:',X0,'ohm')
+
+
+
+    
